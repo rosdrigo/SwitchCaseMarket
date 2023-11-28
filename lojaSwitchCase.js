906153b883878a66a287fdd7bc79574ef2ad49df
Rodrigo <rosdrigol.tech@gmail.com>
2023-11-28 16:03:20 -0300

    Add files via upload

---
+++ lojaSwitchCase.js
@@ -0,0 +1,31 @@
+//Cardápio de uma Loja automatizado em Switch
+
+let produto = "Whey"  //escolha sua Opção! Temos "Banana" "Morango" e "Whey"
+
+
+switch(produto){
+case "Banana":
+console.log("vitamina de banana");
+console.log("vitamina de banana ao leite");
+console.log("Banana Frita");
+console.log("Banana Split");
+break
+case "Morango":
+console.log("vitamina de Morango");
+console.log("vitamina de Morango ao Leite")
+console.log("MC MORANGUINHO");
+console.log("Morango Puro");
+break
+case "Whey":
+console.log("vitamina de Whey");
+console.log("Whey puro com Agua");
+console.log("Sachê de Whey");
+break
+default:
+    console.log("Não temos o produto");
+
+
+
+
+
+}
